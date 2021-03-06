97b707be3c9725e21a2834a22de34d68abd5e0ed
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-06 14:34:35 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  def create
+    
+    @comment = Comment.new(comment_param)
+
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype.id)
+      
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
+  end
+
+  private 
+
+  def comment_param
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
